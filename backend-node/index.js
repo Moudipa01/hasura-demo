45a17cba10143bec01c12d835de2a419264405d8
moudipajanaGrowth-loop <moudipa@growth-loop.io>
2025-12-06 18:14:46 +0530

    project

---
+++ backend-node/index.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const client = axios.create({
+  url: process.env.HASURA_URL,
+  method: "post",
+  headers: {
+    "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET,
+  }
+});
+
+// Run GraphQL
+async function runGraphQL(query, variables = {}) {
+  const res = await axios.post(
+    process.env.HASURA_URL,
+    { query, variables },
+    { headers: { "x-hasura-admin-secret": process.env.HASURA_ADMIN_SECRET } }
+  );
+  console.log(JSON.stringify(res.data, null, 2));
+}
+
+// Insert example
+runGraphQL(`
+mutation MyInsert($title: String!) {
+  insert_tasks_one(object: {title: $title}) {
+    id
+    title
+  }
+}
+`, { title: "Task from Node.js" });
