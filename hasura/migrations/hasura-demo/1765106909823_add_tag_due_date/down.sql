9cc09add53cadd6815d925122b701e5efc72c44d
Moudipa01 <moudipa.jana2020@vitstudent.ac.in>
2025-12-07 20:13:57 +0530

    Add Hasura metadata, migrations, and configuration for tasks management

---
+++ hasura/migrations/hasura-demo/1765106909823_add_tag_due_date/down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.tasks
+DROP COLUMN tag;
+
+ALTER TABLE public.tasks
+DROP COLUMN due_date;
---
+++ hasura/migrations/hasura-demo/1765106909823_add_tag_due_date/up.sql
@@ -0,0 +1,5 @@
+ALTER TABLE public.tasks
+ADD COLUMN tag text;
+
+ALTER TABLE public.tasks
+ADD COLUMN due_date date;
